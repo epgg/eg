93d73e6f789f3f40a111839e722523c35cd39377
Daofeng Li <lidaof@gmail.com>
2015-03-27 00:39:49 -0500

    adding Roadmap browser code

---
+++ browser/roadmap/mm9_hubclusters.js
@@ -0,0 +1,321 @@
+var hubclusters = [
+{
+    "shortname": "ENCODE", 
+    "name": "ENCODE Complete Epigenomes", 
+    "groups": [
+        {
+            "list": [
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10001.encode.hub", 
+                    "name": "MEL"
+                },
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10036.encode.hub", 
+                    "name": "MEF"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10041.encode.hub", 
+                    "name": "ES-Bruce4"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10051.encode.hub", 
+                    "name": "ZhBTc4"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10053.encode.hub", 
+                    "name": "ES-WW6_F1KO"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10002.encode.hub", 
+                    "name": "ES-E14"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10007.encode.hub", 
+                    "name": "Patski"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10059.encode.hub", 
+                    "name": "ES-WW6"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10064.encode.hub", 
+                    "name": "ES-CJ7"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10060.encode.hub", 
+                    "name": "Mesoderm"
+                },
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10027.encode.hub", 
+                    "name": "G1E-ER4"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10030.encode.hub", 
+                    "name": "C2C12"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10004.encode.hub", 
+                    "name": "NIH-3T3"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10066.encode.hub", 
+                    "name": "TReg"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10003.encode.hub", 
+                    "name": "CH12"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10018.encode.hub", 
+                    "name": "B-cell_(CD43-)"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10054.encode.hub", 
+                    "name": "mG/ER"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10025.encode.hub", 
+                    "name": "Erythrobl"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10028.encode.hub", 
+                    "name": "Megakaryo"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10022.encode.hub", 
+                    "name": "T-Naive"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10071.encode.hub", 
+                    "name": "FVLstem"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10026.encode.hub", 
+                    "name": "G1E"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10020.encode.hub", 
+                    "name": "416B"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10067.encode.hub", 
+                    "name": "3134"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10010.encode.hub", 
+                    "name": "A20"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10069.encode.hub", 
+                    "name": "MEP"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10070.encode.hub", 
+                    "name": "FVprogenitor"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10057.encode.hub", 
+                    "name": "THelper-Activated"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10065.encode.hub", 
+                    "name": "TReg-Activated"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10038.encode.hub", 
+                    "name": "BMDM"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10062.encode.hub", 
+                    "name": "EPC_(CD117+_CD71+_TER119-)"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10013.encode.hub", 
+                    "name": "B-cell_(CD19+)"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10029.encode.hub", 
+                    "name": "10T1/2"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10068.encode.hub", 
+                    "name": "Fibroblast"
+                }
+            ], 
+            "name": "ES and Cell Lines"
+        }, 
+        {
+            "list": [
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10075.encode.hub", 
+                    "name": "E14.5-heart"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10076.encode.hub", 
+                    "name": "E14.5-brain"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10077.encode.hub", 
+                    "name": "E14.5-limb"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10078.encode.hub", 
+                    "name": "E14.5-liver"
+                }
+            ], 
+            "name": "Fetal Tissues"
+        },
+        {
+            "list": [
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10023.encode.hub", 
+                    "name": "Lung"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10019.encode.hub", 
+                    "name": "WholeBrain"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10039.encode.hub", 
+                    "name": "OlfactBulb"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10011.encode.hub", 
+                    "name": "Cerebrum"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10009.encode.hub", 
+                    "name": "Liver"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10074.encode.hub", 
+                    "name": "CNS"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10063.encode.hub", 
+                    "name": "ForelimbBud"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10033.encode.hub", 
+                    "name": "Limb"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10006.encode.hub", 
+                    "name": "LgIntestine"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10047.encode.hub", 
+                    "name": "Colon"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10014.encode.hub", 
+                    "name": "Thymus"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10015.encode.hub", 
+                    "name": "HeadlessEmbryo"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10031.encode.hub", 
+                    "name": "Cortex"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10035.encode.hub", 
+                    "name": "SmIntestine"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10012.encode.hub", 
+                    "name": "Spleen"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10016.encode.hub", 
+                    "name": "Heart"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10005.encode.hub", 
+                    "name": "Cerebellum"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10024.encode.hub", 
+                    "name": "Kidney"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10034.encode.hub", 
+                    "name": "BAT"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10056.encode.hub", 
+                    "name": "Retina"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10040.encode.hub", 
+                    "name": "Testis"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10044.encode.hub", 
+                    "name": "Adrenal"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10021.encode.hub", 
+                    "name": "FatPad"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10017.encode.hub", 
+                    "name": "SkMuscle"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10042.encode.hub", 
+                    "name": "MammaryGland"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10032.encode.hub", 
+                    "name": "BoneMarrow"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10037.encode.hub", 
+                    "name": "Placenta"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10008.encode.hub", 
+                    "name": "GenitalFatPad"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10048.encode.hub", 
+                    "name": "Ovary"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10073.encode.hub", 
+                    "name": "Bladder"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10046.encode.hub", 
+                    "name": "Stomach"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10043.encode.hub", 
+                    "name": "Duodenum"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10072.encode.hub", 
+                    "name": "FrontalLobe"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10045.encode.hub", 
+                    "name": "SubcFatPad"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10055.encode.hub", 
+                    "name": "HindlimbBud"
+                }, 
+                {
+                    "url": "http://vizhub.wustl.edu/public/mm9/CompleteEpigenome/10079.encode.hub", 
+                    "name": "Intestine"
+                } 
+            ], 
+            "name": "Adult Tissues"
+        } 
+    ], 
+    "genome": "mm9"
+}
+];
+
