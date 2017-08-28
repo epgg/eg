3681c800be03c913894c32509ff4f6f38378f390
Silas Hsu <hsu.silas@wustl.edu>
2017-08-28 16:08:23 -0500

    fixed URI decoding

--- browser/js/base.js
+++ browser/js/base.js
@@ -1546,14 +1546,15 @@ function parseUrlparam(uph)
 if(window.location.href.indexOf('?')==-1) {
 	return 0;
 }
-var lst = window.location.href.split('?')[1].split('&');
-for(var i=0; i<lst.length; i++) {
-	var t = lst[i].split('=');
-	if(t.length==2 && t[0].length>0 && t[1].length>0) {
-		uph[t[0].toLowerCase()] = t[1];
-	} else {
+var keyValues = window.location.href.split('?')[1].split('&');
+for(let keyValue of keyValues) {
+	let split = keyValue.split('=');
+	if (split.length != 2) {
 		return -1;
 	}
+	let key = decodeURIComponent(split[0]);
+	let value = decodeURIComponent(split[1]);
+	uph[key] = value;
 }
 return 1;
 }
